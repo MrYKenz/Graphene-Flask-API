d1513ea6d92c71ccdc9d17cbe0bb3046f2cdcc86
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-01 00:33:45 +0000

    sqlite database with sqlalchemy

--- main.py
+++ main.py
@@ -1,47 +1,86 @@
 from flask import Flask
 from flask_graphql import GraphQLView
+from flask_sqlalchemy import SQLAlchemy
 import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+import os
 from datetime import datetime
 
 # initialise Flask app
 app = Flask(__name__)
+# setup db
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
-# define users
-class User(graphene.ObjectType):
-    id = graphene.ID()
-    username = graphene.String()
-    last_login = graphene.DateTime()
+# DB Models
+class User(db.Model):
+    __tablename__ = 'users'
+    uuid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), index=True, unique=True)
+    posts = db.relationship('Post', backref='author')
+    def __repr__(self):
+        return '<User %r>' % self.username
+        
+class Post(db.Model):
+    __tablename__ = 'posts'
+    uuid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.uuid'))
+    def __repr__(self):
+        return '<Post %r>' % self.title
 
-# define how user is created
+# Schema Objects
+class UserObject(SQLAlchemyObjectType):
+   class Meta:
+       model = User
+       interfaces = (graphene.relay.Node, )
+
+class PostObject(SQLAlchemyObjectType):
+    class Meta:
+        model = Post
+        interfaces = (graphene.relay.Node, )
+
 class CreateUser(graphene.Mutation):
-    user = graphene.Field(User)
-
-    class Arguments: # takes in arguments from GraphQL Mutation
-        username = graphene.String()
-
+    class Arguments:
+        username = graphene.String(required=True)
+    user = graphene.Field(lambda: UserObject)
     def mutate(self, info, username):
-        # handles Graphql Context
-        # if info.context.get('isAdmin'):
-        #     username = username.upper()
+        user = User.query.filter_by(username=username).first()
         user = User(username=username)
+        db.session.add(user)
+        db.session.commit()
         return CreateUser(user=user)
 
-# Handle Graphql Queries / query schema 
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        body = graphene.String(required=True) 
+        username = graphene.String(required=True)
+    post = graphene.Field(lambda: PostObject)
+    def mutate(self, info, title, body, username):
+        user = User.query.filter_by(username=username).first()
+        post = Post(title=title, body=body)
+        if user is not None:
+            post.author = user
+        db.session.add(post)
+        db.session.commit()
+        return CreatePost(post=post)
+
+# GraphQL Schema
 class Query(graphene.ObjectType):
-    users = graphene.List(User, limit=graphene.Int())
+    node = graphene.relay.Node.Field()
+    all_posts = SQLAlchemyConnectionField(PostObject)
+    all_users = SQLAlchemyConnectionField(UserObject)
 
-    def resolve_users(self, info, limit): # users {} query in GraphQL
-        return [
-            User(username='alpha', last_login=datetime.now()),
-            User(username='beta', last_login=datetime.now()),
-            User(username='delta', last_login=datetime.now()),
-        ][:limit]
-
-# Handle Graphql Mutations / mutation schema
 class Mutations(graphene.ObjectType):
-    create_user = CreateUser.Field() # createUser {} query in GraphQL
+    create_user = CreateUser.Field()
+    create_post = CreatePost.Field()
 
-# Schema for GraphQL Query, Mutations and Subscriptions Classes above
+# Assign Schema for GraphQL Query and Mutation Classes above
 schema = graphene.Schema(query=Query, mutation=Mutations) 
 
 # Flask Graphql Route
@@ -49,4 +88,5 @@ app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=sch
 
 # start here
 if __name__ == "__main__":
+    db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
