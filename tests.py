da830f7571da94f46067fae369b90aa7904d8407
MrYKenz <yasin.kenz@kcl.ac.uk>
2019-12-01 00:46:25 +0000

    graphene tests updated

--- tests.py
+++ tests.py
@@ -1,5 +1,44 @@
-from main import schema
+import graphene
+from datetime import datetime
 import json
+
+# define users
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    last_login = graphene.DateTime()
+
+# define how user is created
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+
+    class Arguments: # takes in arguments from GraphQL Mutation
+        username = graphene.String()
+
+    def mutate(self, info, username):
+        # handles Graphql Context
+        if info.context.get('isAdmin'):
+            username = username.upper()
+        user = User(username=username)
+        return CreateUser(user=user)
+
+# Handle Graphql Queries / query schema 
+class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
+
+    def resolve_users(self, info, limit): # users {} query in GraphQL
+        return [
+            User(username='alpha', last_login=datetime.now()),
+            User(username='beta', last_login=datetime.now()),
+            User(username='delta', last_login=datetime.now()),
+        ][:limit]
+
+# Handle Graphql Mutations / mutation schema
+class Mutations(graphene.ObjectType):
+    create_user = CreateUser.Field() # createUser {} query in GraphQL
+
+# Schema for GraphQL Query, Mutations and Subscriptions Classes above
+schema = graphene.Schema(query=Query, mutation=Mutations) 
 
 result1 = schema.execute(
     '''
